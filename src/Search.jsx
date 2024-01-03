fb3e727242bb43adfddddf3066373d1703c51f37
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-01-03 11:06:02 +0100

    adding functionality on key press

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,6 +1,29 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        //not to clear input field if it is focused
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -8,6 +31,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
